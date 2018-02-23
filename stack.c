5fd4de5859c05cc4f49dc9f1d154ed831d27c7a5
root <root@localhost.localdomain>
2018-02-22 19:50:58 -0500

    file to exploit + a possible solution

---
+++ stack.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+ 
+int main() {
+
+  int exploit = 0x1234;
+  char arr[10];
+ 
+  fgets(arr,13,stdin);
+ 
+  if (exploit == 0x9876) {
+     printf("It worked!\n");
+  } else {
+     printf("It failed.\n");
+  }
+  
+  return 0;
+}
